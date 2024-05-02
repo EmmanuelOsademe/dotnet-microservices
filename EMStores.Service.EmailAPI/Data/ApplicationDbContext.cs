977f227be82488960149d39cff2a351aa75e5107
Emmanuel Osademe <emma.osademe@gmail.com>
2024-05-02 09:48:33 +0100

    email api

---
+++ EMStores.Service.EmailAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using EMStore.Services.EmailAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EMStore.Services.EmailAPI.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    {
+
+        public DbSet<EmailLogger> EmailLoggers { get; set; }
+    }
+}
