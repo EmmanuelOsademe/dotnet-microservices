68a8f4d8270e35e6c9e7514eee6de353e5d72eab
Emmanuel Osademe <emma.osademe@gmail.com>
2024-05-18 09:12:37 +0100

    datatables updated

--- EMStores.Web/wwwroot/js/order.js
+++ EMStores.Web/wwwroot/js/order.js
@@ -1,14 +1,24 @@
+let dataTable;
 $(document).ready(function () {
     loadDataTable();
 });
 
 function loadDataTable() {
-    let dataTable = new DataTable('#tblData');
-    dataTable.dataTable({
-        "ajax": { url: "/order/getall" , type: "GET" },
-        "columns": [
-            { data: 'orderheaderid', "width": "5%"},
-            { data: 'Email', "width": "25%"}
+    /*let dataTable = new DataTable('#tblData');*/
+
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            url: "/order/getall",
+            type: "GET"
+        },
+        columns: [
+            { data: "orderHeaderId", "width": "5%" },
+            { data: "email", width: "10%"},
+            { data: "name", "width": "25%" },
+            { data: "phone", width: "15%" },
+            { data: "status", width: "15%" },
+            { data: "orderTotal", width: "15%" },
+            { data: "_", width: "15%" },
         ]
     })
 }
